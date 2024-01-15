36cdd442e3041decc7b2744cf4df6c74a18950fb
mawu44 <mawuli.05@outlook.com>
2024-01-15 07:39:09 +0000

    added changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("what is your grade percentage?");
+       string answer = Console.Readline();
+      
+       int percent = int.parse(answer);
+       
+       string letter = "";
+
+        if (grade Percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade Percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade Percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade Percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+            Console.WriteLine($"Your letter grade is: {letter}");
+           
+           if (grade Percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time. Keep working hard!");
+    }
+         // Stretch Challenge: 
+        int lastDigit = (int)grade Percentage % 10;
+        string sign = "";
+
+        if (letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        
+
+        // Print the letter grade along with the sign
+        if (sign != "")
+        {
+            Console.WriteLine($"Your detailed grade is: {letter}{sign}");
+        }
     }
 }
