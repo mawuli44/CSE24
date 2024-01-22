39be5a92160b74c945230ada85dc36a2441493a9
mawu44 <mawuli.05@outlook.com>
2024-01-22 10:03:00 +0000

    Commiting program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // The Job class
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
+
+        // Display job details using the Display method
+        job1.Display();
+        job2.Display();
+
+        // New Resume class
+        Resume myResume = new Resume("Allison Rose");
+
+        // Adding jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display resume using Display method
+        myResume.Display();
     }
 }
