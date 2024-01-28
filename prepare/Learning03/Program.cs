c16199b590f0802fe6eed8c88dbf3ef6ea3ef703
mawu44 <mawuli.05@outlook.com>
2024-01-28 19:42:50 +0000

    commit program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+    
+    {
+        // Creating instances using different constructors
+        Fraction fraction1 = new Fraction();      // 1/1
+        Fraction fraction2 = new Fraction(6);    // 6/1
+        Fraction fraction3 = new Fraction(6, 7);  // 6/7
+
+        // Displaying initial fractions
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        // Modifying fractions using setters
+        fraction1.SetNumerator(5);
+        fraction2.SetDenominator(3);
+
+        // Displaying modified fractions
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
     }
-}
+} 
