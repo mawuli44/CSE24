3cb5d419d8838c9b228a8b3790f569d3a26a96e6
mawu44 <mawuli.05@outlook.com>
2024-02-08 15:26:02 +0000

    Added changes to program.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Create a list to hold shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Addind  square, rectangle, and circle to the list
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Iterating through the list of shapes
+        foreach (Shape shape in shapes)
+        {
+            // Display color and area of each shape
+            Console.WriteLine($"Color: {shape.Color}, Area: {shape.GetArea()}");
+        }
     }
 }
