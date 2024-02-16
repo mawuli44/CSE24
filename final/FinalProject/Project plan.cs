054a56a55ed5526dffe33e089efb91083c93bd43
mawu44 <mawuli.05@outlook.com>
2024-02-16 19:43:00 +0000

    Added final project plan

---
+++ final/FinalProject/Project plan.cs
@@ -0,0 +1,6 @@
+Project plan (open-ended project)
+The plan of my project is to Create and design a system thus home automated system 
+that will allow users to control various aspects of their home such as lighting, temperature, 
+security cameras and appliances remotely through a web or mobile interface. 
+Sensors would be incorporated to automate certain tasks based on environmental conditions or user preferences.
+The program shall be written in C# and all specifications and guidelines associated.
